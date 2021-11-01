4209039061820fc85d8981376fddf992d49b8000
Estefanía Pedrero <estefania.fsd108@gmail.com>
2021-11-01 11:28:24 +0100

    Iteration#4

---
+++ Iteration4.js
@@ -0,0 +1,13 @@
+'use strict'
+/*Usa un **for...in** para imprimir por consola los datos del alienígena.. Puedes usar este objeto:*/
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for ( let propiedad in alien){
+    console.log(`${alien[propiedad]}`);
+}
